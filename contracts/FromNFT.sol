07cbce9de24ef01990750b066aaba0ac16cec155
Du Huynh <duhq@bap.jp>
2022-07-01 18:33:23 +0700

    Support TokenURI

--- contracts/FromNFT.sol
+++ contracts/FromNFT.sol
@@ -2,8 +2,19 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract FromNFT is ERC721Burnable, Ownable {
+contract FromNFT is ERC721URIStorage, ERC721Burnable, Ownable {
     constructor() ERC721("FromNFT", "FROM") {}
+
+    function _burn(uint256 tokenId) internal virtual override(ERC721URIStorage, ERC721) {
+        // Need test to ensure it will call ERC721URIStorage._burn function.
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId) public view virtual override(ERC721URIStorage, ERC721) returns (string memory) {
+        // Need test to ensure it will call ERC721URIStorage.tokenURI function.
+        return super.tokenURI(tokenId);
+    }
 }
