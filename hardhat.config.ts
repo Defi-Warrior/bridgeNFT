6b8e139515e42967e4ad9411c9c4510f5288933a
Du Huynh <duhq@bap.jp>
2022-07-26 13:33:39 +0700

    Add networks

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,5 +3,20 @@ import '@nomiclabs/hardhat-ethers'
 import '@typechain/hardhat'
 
 export default {
-    solidity: "0.8.4"
+    solidity: "0.8.4",
+    defaultNetwork: "localhost",
+    networks: {
+        hardhat: {},
+        localhost: {
+            url: "http://127.0.0.1:8545"
+        },
+        bsctest: {
+            url: "https://data-seed-prebsc-2-s1.binance.org:8545/",
+            accounts: [process.env.PRIVATE_KEY],
+        },
+        bscmainnet: {
+            url: "https://bsc-dataseed.binance.org/",
+            accounts: [process.env.PRIVATE_KEY],
+        }
+    }
 };
