5020dac485bbe558b76eef8fa6564cf791dbc50b
Du Huynh <duhq@bap.jp>
2022-08-09 09:46:57 +0700

    Add announce revocation to single contract.

--- contracts/from/AbstractRevocableFromBridge.sol
+++ contracts/from/AbstractRevocableFromBridge.sol
@@ -4,11 +4,18 @@ pragma solidity ^0.8.0;
 import "./interfaces/IRevocableFromBridge.sol";
 import "./AbstractFromBridge.sol";
 
+import "./interfaces/IValidatorRevocationAnnouncementReceiver.sol";
+
 /**
  * @title AbstractRevocableFromBridge
  * @dev The version of FromBridge that supports revoking validator then switching to a new one.
  */
 abstract contract AbstractRevocableFromBridge is IRevocableFromBridge, AbstractFromBridge {
+    /**
+     * @dev Max gas for receiver contract can consume to update state.
+     */
+    uint256 private constant _MAX_ANNOUNCEMENT_GAS = 30000;
+
     /**
      * @dev See IRevocableFromBridge.
      * Child contracts MAY override this function if they want further fine-grained control
@@ -19,16 +26,14 @@ abstract contract AbstractRevocableFromBridge is IRevocableFromBridge, AbstractF
         // Check all validator requirements.
         _checkValidatorRequirements(newValidator);
 
-        address revokedValidator = _validator;
+        // Announe revocation to other contracts.
+        _announceRevocationToAll(_validator, newValidator, msg.sender);
+
+        // Emit event.
+        emit Revoke(_validator, newValidator, msg.sender, block.timestamp);
 
         // Update validator.
         _validator = newValidator;
-
-        // Announe revocation to other contracts.
-        _announceRevocation(revokedValidator, newValidator, msg.sender);
-
-        // Emit event.
-        emit Revoke(revokedValidator, newValidator, msg.sender, block.timestamp);
     }
 
     /**
@@ -36,13 +41,49 @@ abstract contract AbstractRevocableFromBridge is IRevocableFromBridge, AbstractF
      * take proper subsequent actions if needed. Child contracts MAY override to add the
      * announcement(s) they need.
      *
-     * This function SHOULD NOT propagate the exceptions thrown by the called contracts to not
-     * interfere with the revocation. Also, an announcement to a contract SHOULD not interfere with
-     * announcements to other contracts as well. Therefore, there SHOULD be a try-catch block
-     * that swallows exception for each external call. Or alternatively, use the low-level
-     * "address.call".
+     * The receivers (i.e. announcement receiving contracts) MUST implement the
+     * "IValidatorRevocationAnnouncementReceiver" interface.
+     *
+     * When overriding, the "_announceRevocationTo" function MUST be used to announce to each receiver.
+     * Child contracts MAY optionally implement observer pattern to get more dynamic control over
+     * the receiver list.
      */
-    function _announceRevocation(address revokedValidator, address newValidator, address revoker) internal virtual {}
+    function _announceRevocationToAll(
+        address revokedValidator,
+        address newValidator,
+        address revoker
+    ) internal virtual {}
+
+    /**
+     * @dev See "_announceRevocationToAll" function.
+     * Announce the revocation to one contract.
+     *
+     * This function do not propagate the exception thrown by the called contract to not
+     * interfere with the revocation, and also, to the announcement to other contracts.
+     * Therefore, low-level call is used to swallow exception.
+     *
+     * The gas is specified to prevent the receiver from consuming too much gas and
+     * affect the revocation transaction. For this reason, the receiver might fail to
+     * update its state to reflect the revocation and might lead to security issues.
+     * This is a dilemma between revocation success and announcement success. We chose
+     * to prioritized the revocation.
+     */
+    function _announceRevocationTo(
+        address receiver,
+        address revokedValidator,
+        address newValidator,
+        address revoker
+    ) internal {
+        require(gasleft() >= _MAX_ANNOUNCEMENT_GAS, "revokeValidator: Not enough gas to announce to all");
+
+        receiver.call{ gas: _MAX_ANNOUNCEMENT_GAS }( abi.encodeWithSelector(
+            IValidatorRevocationAnnouncementReceiver.receiveAnnouncement.selector,
+            revokedValidator,
+            newValidator,
+            revoker) );
+    }
+
+    /* ********************************************************************************************** */
 
     /**
      * @dev See AbstractFromBridge.
--- contracts/to/tobridges/defi-warrior/interfaces/INFTManager.sol
+++ contracts/to/tobridges/defi-warrior/interfaces/INFTManager.sol
@@ -8,3 +8,33 @@ interface INFTManager {
         uint32[20][6] memory bodypartAttributes
     ) external returns(uint256);
 }
+
+/* NFTManager example
+
+contract NFTManager is INFTManager {
+    // Add setters require admin right.
+    address _toBridge;
+    bool _allowMint;
+
+    function receiveAnnouncement(address revokedValidator, address newValidator, address revoker) external {
+        require(msg.sender == _toBridge);
+        _allowMint = false;
+        // ...
+    }
+
+    function isCurrentlyMintable() external view returns(bool) {
+        // Could be other logic.
+        return _allowMint;
+    }
+
+    function mint(
+        address to,
+        uint32[30] memory warriorAttributes,
+        uint32[20][6] memory bodypartAttributes
+    ) external returns(uint256) {
+        require(msg.sender == _toBridge);
+        require(_allowMint);
+        // ...
+    }
+}
+*/
